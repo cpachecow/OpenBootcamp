41530aa0109d0a4ce4a716d2a923ab1213d2d844
cpachecow <109570000+cpachecow@users.noreply.github.com>
2022-07-19 23:35:54 -0400

    Create ejercicio1.py

---
+++ Python/03/ejercicio1.py
@@ -0,0 +1,9 @@
+Nombre = "Cristian"
+Apellidos = "Pacheco Wajner"
+Edad = 41
+Email = "cpachecow@gmail.com"
+telefono = "+56968954869"
+casado = true
+con_hijos = true
+amigos = ['Carolina', 'Angela', 'Sergio', 'Cristian']
+peliculas = {'pelicula1':'Terminator','pelicula2':'LOTR','pelicula3':'Silencio de Los Inocentes','pelicula4':'Interestelar'}
