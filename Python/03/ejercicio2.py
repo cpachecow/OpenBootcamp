337222764d868413f0a3b3edb3faf831272d3ed9
cpachecow <109570000+cpachecow@users.noreply.github.com>
2022-07-21 14:02:26 -0400

    Update ejercicio2.py

--- Python/03/ejercicio2.py
+++ Python/03/ejercicio2.py
@@ -1,4 +1,7 @@
-peso = input('Favor indicar tu peso: ')
-estatura = input('Favor indicar tu estatura (en metros ej 1,62): ')
+peso = float(input('Favor indicar tu peso: '))
+estatura = float(input('Favor indicar tu estatura (en metros ej 1,62): '))
 imc = (peso/estatura)
-print "Su IMC es : " + imc
+imc = "%.2f" % imc # dando formato de dos decimales al número decimal
+imc = str(imc)     # pasando de float a string, para poderlo imprimir en pantalla
+
+print ("Su IMC es : " + imc)
