b79458545e7e667e5c5df951cd09ab01047ebfeb
cpachecow <109570000+cpachecow@users.noreply.github.com>
2022-07-24 22:34:24 -0400

    Create ejercicio1.py

---
+++ Python/04/ejercicio1.py
@@ -0,0 +1,9 @@
+# Tarea1: Indicar si una edad ingresada por pantalla
+#         corresponde a un mayor de esas
+
+edad = int(input('Favor indicar tu edad: '))
+
+if(edad >= 18):
+     print("Eres mayor de edad")
+elif(edad < 18):
+    print("Eres menor de edad")
