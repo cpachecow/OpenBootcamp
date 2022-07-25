208550519c72549b2dc4eb815a894d977c107e3a
cpachecow <109570000+cpachecow@users.noreply.github.com>
2022-07-24 23:08:17 -0400

    Add files via upload

---
+++ Python/04/ejercicio3.py
@@ -0,0 +1,16 @@
+# Tarea3: Escribe un programa que sea capaz de mostrar
+#         los números del 1 al 100 en orden inverso.
+
+num1 = 1
+num2 = 100
+numeros = []
+
+while (num1 <= num2):
+
+    numeros.append(num2)  
+    num2 -= 1
+
+print("\nNúmeros:\n\n",numeros)
+
+    
+    
