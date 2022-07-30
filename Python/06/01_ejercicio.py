8a35836dad7ad4af2a7ba0bc30394604e1b7a17d
cpachecow <109570000+cpachecow@users.noreply.github.com>
2022-07-30 17:38:53 -0400

    Create 01_ejercicio.py

---
+++ Python/06/01_ejercicio.py
@@ -0,0 +1,20 @@
+# Ejercicio 1, clase class Auto: 
+
+class Vehiculo():
+
+    color = "Azul"
+    ruedas = 4
+    puertas = 4
+
+class Coche(Vehiculo):
+
+    velocidad = "300 km/h"
+    cilindrada = "3.5"
+
+
+a = Coche()
+print("Las características de mi Coche:")
+print("Color:", a.color)
+print("Ruedas:", a.ruedas)
+print("Velocidad:", a.velocidad)
+print("Puertas:", a.puertas)
