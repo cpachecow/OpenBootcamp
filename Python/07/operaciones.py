94d345644a06d6f9a2b51fad4ae39730d91a47ff
cpachecow <109570000+cpachecow@users.noreply.github.com>
2022-11-15 23:55:12 -0300

    Add files via upload

---
+++ Python/07/operaciones.py
@@ -0,0 +1,32 @@
+# Ejercicio 1, clase Módulos:
+
+# Módulo que tendrá funciones y serán llamadas de un archivo ppal.
+
+# Se puede incluir variables que no cambiarán y se ecriben en MAYUSCULA.
+
+PI = 3.141516
+UF = 30000
+DOLAR = 994
+EURO = 900
+
+def suma(a,b):
+    return a + b
+
+def resta(a,b):
+    return a - b
+
+def producto(a,b):
+    return a * b
+
+def division(a,b):
+    return a/b
+
+def potencia(a,b):
+    return a**b
+
+
+
+
+
+
+
